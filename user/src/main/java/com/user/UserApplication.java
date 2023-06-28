1f0f5256a47b34dfae82dc8c18e3e1367086e79a
yudonggeun <ydong98@gmail.com>
2023-06-28 15:30:32 +0900

    setting user module

--- user/src/main/java/com/user/UserApplication.java
+++ user/src/main/java/com/user/UserApplication.java
@@ -1,9 +1,11 @@
-package config;
+package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
